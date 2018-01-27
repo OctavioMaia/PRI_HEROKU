889050748ff68e5626982211ecedb936bb884b49
Octávio Maia <a71369@alunos.uminho.pt>
2018-01-27 22:42:34 +0000

    fix auth

--- config/auth.js
+++ config/auth.js
@@ -6,8 +6,8 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '772972342897636', // your App ID
         'clientSecret'    : 'b9915731ca479e3cc7675dfe9e650e38', // your App Secret
-        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
-        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        'callbackURL'     : 'https://pri2017.herokuapp.com/auth/facebook/callback',
+        'profileURL'      : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields'   : ['id', 'email', 'name'] // For requesting permissions from Facebook API
 
     },
@@ -15,7 +15,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'         : '142061558149-lpfuh54k06shg9vu3fee9qqkp19t1rss.apps.googleusercontent.com',
         'clientSecret'     : 'PFmCiyiU8UU7iTRKM4p7k8Md',
-        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'      : 'https://pri2017.herokuapp.com/auth/google/callback'
     },
 
     'secretToken' : 'pri2017'
